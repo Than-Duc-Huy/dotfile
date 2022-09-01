7ec3d14dac4fffb5af9de0a19b0df6383a940194
huy_huy <e0978364@u.nus.edu>
2022-09-01 10:36:59 +0800

    Change toml

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
+
+
+# git clone dotfile in HOME directory
+
+# Install Hack font
 curl -L -O https://raw.githubusercontent.com/source-foundry/hack-linux-installer/master/hack-linux-installer.sh
 chmod +x hack-linux-installer.sh
 ./hack-linux-installer.sh latest
-yes Y | curl -ss https://starship.rs/install.sh | sh
+
+
+# Install starship
+yes y | curl -ss https://starship.rs/install.sh | sh
+
+
+
+# Move starship.toml file to .config
 mv ~/dotfile/starship.toml ~/.config/starship.toml
+
+
+# Add this line to .bashrc
 echo "eval \"$(starship init bash)\"" >> ~/.bashrc
