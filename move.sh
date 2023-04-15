4ed607ae5cc0ee639856b38d479f737b81f06d45
td_huy <than_duc_huy@outlook.com>
2023-04-15 18:40:26 +0100

    move

--- move.sh
+++ move.sh
@@ -4,4 +4,4 @@ rm ~/.config/starship.toml
 ln -s ~/dotfile/starship.toml ~/.config/starship.toml
 
 # Add this line to .bashrc
-echo "\neval \"\$(starship init bash)\" \n" >> ~/.bashrc
+echo "eval \"\$(starship init bash)\"" >> ~/.bashrc
